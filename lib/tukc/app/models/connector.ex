5c29a6c87f1b7a9b59d5a8ece05b6f6b1b0d3aec
Jason Schweier <jason.schweier@gmail.com>
2021-01-31 10:06:11 -0500

    Delete unused models

--- lib/tukc/app/models/connector.ex
+++
@@ -1,8 +0,0 @@
-defmodule Tukc.App.Models.Connector do
-  @enforce_keys [:cluster, :name]
-  defstruct [:cluster, :name]
-
-  def new(name, cluster) do
-    %__MODULE__{name: name, cluster: cluster}
-  end
-end
